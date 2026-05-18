8e72bb2f77b6772d09758714c299204d4ab2a9ce
Haolader91 <haolader910@gmail.com>
2026-05-18 23:41:59 +0600

    save submitted data to mongodb

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const dotenv = require("dotenv");
-
+const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 dotenv.config();
 const uri = process.env.MONGODB_URI;
 const app = express();
 const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(cors());
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -18,12 +21,23 @@ async function run() {
   try {
     await client.connect();
 
+    const db = client.db("StudyNook");
+
+    const roomsCollection = db.collection("rooms");
+
+    app.post("/rooms", async (req, res) => {
+      const requestRoomsData = req.body;
+      console.log(requestRoomsData);
+      const result = await roomsCollection.insertOne(requestRoomsData);
+      res.json(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!",
     );
   } finally {
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
